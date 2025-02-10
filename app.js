ca029c3b1d4dfc62fa9ab334528395f667ddae16
Asia Peters <asia.peters@snhu.edu>
2025-02-09 21:44:28 -0700

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// Define routers
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
-var mealsRouter = require("./app_server/routes/meals");
-var newsRouter = require("./app_server/routes/news");
 var roomsRouter = require("./app_server/routes/rooms");
-var contactRouter = require("./app_server/routes/contact");
+var mealsRouter = require("./app_server/routes/meals");
 var aboutRouter = require("./app_server/routes/about");
+var contactRouter = require("./app_server/routes/contact");
+var newsRouter = require("./app_server/routes/news");
+var apiRouter = require("./app_api/routes/index");
+
 var handlebars = require("hbs");
+
+// Bring in the database
+require("./app_api/models/db");
 
 var app = express();
 
@@ -20,7 +26,9 @@ var app = express();
 app.set("views", path.join(__dirname, "app_server", "views"));
 
 // register handlebars partials
-handlebars.registerPartials(__dirname + "/app_server/views/partials");
+handlebars.registerPartials(
+  path.join(__dirname, "app_server", "views", "partials")
+);
 
 app.set("view engine", "hbs");
 
@@ -30,30 +38,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// wire-up routes to the controllers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
-app.use("/meals", mealsRouter);
-app.use("/news", newsRouter);
 app.use("/rooms", roomsRouter);
-app.use("/contact", contactRouter);
+app.use("/meals", mealsRouter);
 app.use("/about", aboutRouter);
-
+app.use("/contact", contactRouter);
+app.use("/news", newsRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,36 @@
-var fs = require('fs'); 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
-/* Get travel view */
-const travel = (req, res) => {
-    res.render("travel", { title: "Travlr Getaways", trips });
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+  method: "GET",
+  headers: { 
+    'Accept': "application/json"
+  },  
+};
+
+/* GET travel view */
+const travel = async function(req, res, next) {
+  // console.log('Travel Controller Begin');
+  await fetch(tripsEndpoint, options)
+  .then(res => res.json())
+  .then(json => {
+    // console.log(json);
+    let message = null;
+    if(!(json instanceof Array)) {
+      message = 'API lookuo error';
+      json = [];
+    } else {
+        if(!json.length) {
+          message = 'No trips exists in our database!'
+        }
+    }
+    res.render("travel", { title: "Travlr Getaways", trips: json, message });
+  })
+  .catch(err => res.status(500).send(err.message));
+  // console.log('Travel Controller after render');
 };
 
 module.exports = {
-    travel
+  travel,
 };
