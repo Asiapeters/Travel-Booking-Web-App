e0f7b0294b196fc4ba6ba226976468eec14c6664
Asia Peters <asia.peters@snhu.edu>
2025-02-23 21:56:21 -0700

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const tripsController = require("../controllers/trips");
 
 // define route for our trips endpoint
-router.route("/trips").get(tripsController.tripslist);
+router
+  .route("/trips")
+  .get(tripsController.tripslist)
+  .post(tripsController.tripsAddTrip);
 
 // GET Method routes tripsFindByCode - requires parameter
-router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
+// PUT Method routes tripUpdateTrip - requires parameter
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsFindByCode)
+  .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
